fa7b52bce05d47424d3ab82cf1335f8f15eaeeea
Simon Bruguerolle <simon.bruguerolle@hotmail.fr>
2018-01-12 17:09:50 +0100

    Add of route change on scroll

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,48 @@
 </template>
 
 <script>
+  import debounce from 'debounce'
+  import { routes } from './router'
+  let step = 0
 
-export default {
-  name: 'app',
-  mounted: function() {
-    setTimeout(() => {
-      this._self.$el.nextElementSibling.classList.value += ' loaded';
-    }, 1000);
+  export default {
+    name: 'app',
+    mounted: function() {
+      setTimeout(() => {
+        this._self.$el.nextElementSibling.classList.value += ' loaded';
+      }, 1000);
+      window.addEventListener('wheel', debounce(this.handleScroll, 800, true));    
+    },
+    methods: {
+      handleScroll (event) {
+        event.deltaY < 0 ? this.goBack() : this.goForward()
+      },
+      goForward () {
+        this.$router.push(routes[step].path);
+        step++
+      },
+      goBack () {
+        if(window.history.length > 1) {
+          this.$router.go(-1)
+          step--
+        }
+      }
+    },
+    destroyed () {
+      window.removeEventListener('wheel', this.handleScroll);
+    }
   }
-}
 </script>
 
 <style lang="scss">
-
+#app {
+    background: url('./assets/images/paris-bg.jpg');
+    position: absolute;
+    top: 0;
+    left: 0;
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 100%;
+}
 </style>
--- src/router.js
+++ src/router.js
@@ -1,5 +1,7 @@
 module.exports = {
     routes: [
-        { path: '/', component: require('./components/Home.vue').default  }
+        { path: '/home', component: require('./components/Home.vue').default  },
+        { path: '/setUp', component: require('./components/Guide.vue').default  },
+        { path: '/plan', component: require('./components/Route.vue').default  }
     ]
 };
