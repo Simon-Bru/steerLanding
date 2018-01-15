50b98bb8eedfaa574f55a5d5b1a7e651af2fa1f1
Simon Bruguerolle <simon.bruguerolle@hotmail.fr>
2018-01-15 16:55:42 +0100

    semi-columns add

--- src/main.js
+++ src/main.js
@@ -1,26 +1,26 @@
-import Vue            from 'vue'
-import VueI18n        from 'vue-i18n'
-import VueRouter      from 'vue-router'
-import App            from './App'
-import { routes }     from './router'
-import messages       from './locale'
-import './assets/style/style.scss'
+import Vue            from 'vue';
+import VueI18n        from 'vue-i18n';
+import VueRouter      from 'vue-router';
+import App            from './App';
+import { routes }     from './router';
+import messages       from './locale';
+import './assets/style/style.scss';
 
-Vue.config.productionTip = false
-Vue.use(VueI18n)
-Vue.use(VueRouter)
+Vue.config.productionTip = false;
+Vue.use(VueI18n);
+Vue.use(VueRouter);
 
 const i18n = new VueI18n({
   locale: window.navigator.language,
   messages
-})
+});
 
 const router = new VueRouter({
   mode: 'history',
   routes
-})
+});
 
-router.replace({ path: '/', redirect: '/' })
+router.replace({ path: '/', redirect: '/' });
 
 new Vue({
   el: '#app',
@@ -28,4 +28,4 @@ new Vue({
   i18n,
   router,
   components: { App }
-})
+});
