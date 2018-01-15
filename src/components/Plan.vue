7ea45a9fb96458aa7baed43f4686b214ed633adb
Simon Bruguerolle <simon.bruguerolle@hotmail.fr>
2018-01-15 14:46:20 +0100

    add basic layout

--- src/components/Plan.vue
+++ src/components/Plan.vue
@@ -1,14 +1,33 @@
 <template>
   <div id="plan">
-      <h1>Plan</h1>
+    <section class="descContainer">
+        <h1 v-html="$t('message2.title')"></h1>
+        <p v-html="$t('message2.desc')"></p>
+    </section>
+    <section>
+        <ui-frame step="1"></ui-frame>
+    </section>
   </div>
 </template>
 
 <script>
+import UiFrame from './UiFrame';
+
 export default {
-    name: 'Plan'
+    name: 'Plan',
+    components: {
+        UiFrame
+    }
 }
 </script>
 
 <style lang="scss">
+#plan {
+    display: grid;
+    grid-template-columns: 2fr 1fr;
+    grid-auto-rows: 100%;
+    padding-bottom: 20px;
+    height:100%;
+    text-align: left;
+}
 </style>
