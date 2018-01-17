6b84d39be4d268c77caeb8ab68ecff899f4eff7a
Simon Bruguerolle <simon.bruguerolle@hotmail.fr>
2018-01-17 23:17:15 +0100

    Svg paths add

--- src/components/ActivityMap.vue
+++ src/components/ActivityMap.vue
@@ -7,7 +7,37 @@
         <span class="circle"></span>
     </span>
 
-    <i class="icons8-location-marker" :class="activityClass"></i>
+    <svg 
+        xmlns="http://www.w3.org/2000/svg" 
+        viewBox="0 0 80 135" 
+        preserveAspectRatio="none">
+
+        <polyline id="path1"
+        points="24, 66 30, 68 29, 80" 
+        stroke="black" 
+        fill="none"
+        style="stroke-width: 1.3px"></polyline>
+
+        <polyline id="path2"
+        points="29, 80 30, 65 33, 59 46, 55 50, 47 56, 52 60, 55 63, 56" 
+        stroke="black" 
+        fill="none"
+        style="stroke-width: 1.3px"></polyline>
+
+        <polyline id="path3"
+        points="57, 53 61, 44 50, 39 43, 41"
+        stroke="black" 
+        fill="none"
+        style="stroke-width: 1.3px"></polyline>
+
+        <!-- Points -->
+        <circle cx="29" cy="80" r="1.5" fill="red"/>
+        <circle cx="63" cy="56" r="1.5" fill="red"/>
+        <circle cx="43" cy="41" r="1.5" fill="red"/>
+    </svg>
+
+    <i  class="icons8-location-marker" 
+        id="marker"></i>
 
     <div id='activityContainer'>
         <div class="button flex" id="no"><span v-html="$t('no')"></span><i class="icons8-no"></i></div>
@@ -40,16 +70,15 @@ export default {
         });
         return {
             activities: array,
-            activityNb: 0,
-            activityClass: "activity0"
+            activityNb: 0
         };
     },
     mounted: function() {        
         const localization = document.querySelector('#localization .circle');      
-        // TweenMax.to(localization, 1, 
-        // { width: '10vh', height: '10vh', 
-        // repeat: -1, 
-        // yoyo: true }).play();
+        TweenMax.to(localization, 1, 
+        { width: '10vh', height: '10vh', 
+        repeat: -1, 
+        yoyo: true }).play();
 
 
         let tweenline = new TimelineLite({paused: true});
@@ -57,17 +86,36 @@ export default {
         tweenline.to(document.getElementById('4'), .5, 
                     { left: '100%', ease: Back.easeIn, delay: 3.5, onComplete: this.nextActivity })
                 .to(document.getElementById('3'), .5, 
-                    { right: '100%', ease: Back.easeIn, delay: 1, onComplete: this.nextActivity })
+                    { right: '100%', ease: Back.easeIn, delay: 2, onComplete: this.nextActivity })
                 .to(document.getElementById('2'), .5, 
-                    { left: '100%', ease: Back.easeIn, delay: 1, onComplete: this.nextActivity })
+                    { left: '100%', ease: Back.easeIn, delay: 2, onComplete: this.nextActivity })
                 .to(document.getElementById('1'), .5, 
-                    { left: '100%', ease: Back.easeIn, delay: 1, onComplete: this.nextActivity }).play();
+                    { left: '100%', ease: Back.easeIn, delay: 2, onComplete: this.nextActivity }).play();
 
     },
     methods: {
-        nextActivity: () => {
+        nextActivity: function() {
             this.activityNb++;
-            this.activityClass = 'activity'+this.activityNb;
+            const marker = document.getElementById('marker');
+            switch(this.activityNb) {
+                case 1: 
+                    marker.style.top = '60%';
+                    marker.style.left = '85%';
+                break;
+                case 2: 
+                    marker.style.top = '32%';
+                    marker.style.left = '70%';
+                break;
+                case 3: 
+                    marker.style.top = '20%';
+                    marker.style.left = '40%';
+                break;
+                case 4: 
+                    marker.style.top = '21%';
+                    marker.style.left = '76%';
+                break;
+                
+            }
         }
     },
     components: {
@@ -84,7 +132,8 @@ export default {
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
-    background-position-x: 40%;  
+    background-position-x: 56%;
+    background-position-y: 20%;
     width: 100%;
     height: 100%;
 
@@ -93,24 +142,24 @@ export default {
         margin: 10px;
     }
 
-    .icons8-location-marker {
+    #marker {
         position: absolute;
         color: $pink;
-    }
-    .activity0 {
-        left: 75%;
-        top: 55%;
+        font-size: 4vh;
+        top: 52%;
+        left: 34%;
     }
 
 
     #localization {
         position: absolute;
-        top: 46%;
-        left: 65%;
+        top: 48%;
+        left: 28%;
         width:  2vh;
         height: 2vh;
         justify-content: center;
         align-items: center;
+        z-index: 100;
 
         .circle {
             position: absolute;
@@ -129,6 +178,13 @@ export default {
             box-shadow: 0 1px 6px 1px $lightblue;
 
         }
+    }
+
+    svg {
+        position: absolute;
+        top: 0; left: 0;
+        width: 100%;
+        height: 100%;
     }
 
 
