308eb6136b5f96b64c2b3aaff896bd418c331ef1
Simon Bruguerolle <simon.bruguerolle@hotmail.fr>
2018-01-17 20:42:39 +0100

    fix

--- src/locale.js
+++ src/locale.js
@@ -37,31 +37,31 @@ module.exports = {
                 name:       'Brasserie',
                 location:   'address',
                 time:       '9:30 PM - 11:00 PM',
-                distance:   '2km'    
+                distance:   '2km'
             },
             {
                 name:       'Bar à vins',
                 location:   'Petit Palais, aris 75007',
                 time:       '6:30 PM - 9:00 PM',
-                distance:   '2km'    
+                distance:   '2km'
             },
             {
                 name:       'Balade dans les tuileries',
                 location:   'Jardin des tuileries, Paris 75007',
                 time:       '6:20 PM - 8:00 PM',
-                distance:   '3km'    
+                distance:   '3km'
             },
             {
                 name:       'Boulangerie artisanale',
                 location:   'Petit Palais, ',
                 time:       '5:15 PM - 5:45PM',
-                distance:   '1km'    
+                distance:   '1km'
             },
             {
                 name:       'Exposition Grèce antique',
                 location:   'Petit Palais, Paris 75007',
                 time:       '3:00 PM - 5:00 PM',
-                distance:   '450m'    
+                distance:   '450m'
             }
         ]
     }
