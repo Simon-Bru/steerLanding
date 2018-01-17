6bbd572d1b4f8bf20613add014a9d3d2a1ec792a
Simon Bruguerolle <simon.bruguerolle@hotmail.fr>
2018-01-17 20:32:49 +0100

    Add of activities

--- src/locale.js
+++ src/locale.js
@@ -1,7 +1,7 @@
 
 module.exports = {
     en: {
-        yes:    'Let\'s go !',
+        yes:    'Let\'s go',
         no:     'No thanks',
         message1: {
             title:  'Save your olympic performances',
@@ -17,7 +17,7 @@ module.exports = {
         }
     },
     fr: {
-        yes:    'J\'y vais !',
+        yes:    'J\'y vais',
         no:     'Non merci',
         message1: {
             title:  'Enregistrez vos performances olympiques',
@@ -32,11 +32,37 @@ module.exports = {
             desc:   'Choisissez les activités auquelles vous souhaitez participer et Steer organise '+
                     'votre parcours.'
         },
-        activity1: {
-            name:       'Exposition Grèce Antique',
-            location:   'Petit Palais, Paris 75007',
-            time:       '3:00 PM - 5:00 PM',
-            distance:   '450m'
-        }
+        activities: [
+            {
+                name:       'Brasserie',
+                location:   'address',
+                time:       '9:30 PM - 11:00 PM',
+                distance:   '2km'    
+            },
+            {
+                name:       'Bar à vins',
+                location:   'Petit Palais, aris 75007',
+                time:       '6:30 PM - 9:00 PM',
+                distance:   '2km'    
+            },
+            {
+                name:       'Balade dans les tuileries',
+                location:   'Jardin des tuileries, Paris 75007',
+                time:       '6:20 PM - 8:00 PM',
+                distance:   '3km'    
+            },
+            {
+                name:       'Boulangerie artisanale',
+                location:   'Petit Palais, ',
+                time:       '5:15 PM - 5:45PM',
+                distance:   '1km'    
+            },
+            {
+                name:       'Exposition Grèce antique',
+                location:   'Petit Palais, Paris 75007',
+                time:       '3:00 PM - 5:00 PM',
+                distance:   '450m'    
+            }
+        ]
     }
 }
