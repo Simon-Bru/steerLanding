fb1fb0131edfc2c4d59eac30a2875b6bc37f53cf
Simon Bruguerolle <simon.bruguerolle@hotmail.fr>
2018-01-19 11:25:57 +0100

    remove route

--- src/router.js
+++ src/router.js
@@ -2,7 +2,6 @@ module.exports = {
     routes: [
         { path: '/start',       component: require('./components/Start.vue').default  },
         { path: '/plan',        component: require('./components/Plan.vue').default  },
-        { path: '/route',       component: require('./components/Route.vue').default  },
         { path: '/contact',     component: require('./components/Contact.vue').default  }
     ]
 };
