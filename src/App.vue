f85e9b1eccd819c1d1ddae050fd31c2a1bbe2f82
Simon Bruguerolle <simon.bruguerolle@hotmail.fr>
2018-01-19 16:04:59 +0100

    build fix

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <video autoplay muted loop id="Parisvideo">
-      <source src="/src/assets/images/Paris.mp4" type="video/mp4">
+      <source src="src/assets/images/Paris.mp4" type="video/mp4">
       Your browser does not support HTML5 video.
     </video>
     
